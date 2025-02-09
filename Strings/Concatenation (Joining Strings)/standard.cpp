4326a3ac0e7bb322456179a4d878dec418326a74
Mahima Chavalam <sussybakka69696@gmail.com>
2025-02-09 17:57:54 +0530

    Create standard.cpp

---
+++ Strings/Concatenation (Joining Strings)/standard.cpp
@@ -0,0 +1,19 @@
+# v Using + Operator
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string first = "Hello";
+    string second = " World";
+    
+    string full = first + second; // Concatenation using +
+    cout << full << endl;
+    return 0;
+}
+
+
+
+✅ Shortcut: str1 += str2; appends str2 to str1.
