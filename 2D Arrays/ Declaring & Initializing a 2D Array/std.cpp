6b0f7efd720906c108757b9265700bf0bbd2e1af
Mahima Chavalam <sussybakka69696@gmail.com>
2025-02-09 18:12:28 +0530

    Create std.cpp

---
+++ 2D Arrays/ Declaring & Initializing a 2D Array/std.cpp
@@ -0,0 +1,10 @@
+General Syntax:
+datatype arrayName[rows][columns];
+
+#  Example: Declaring a 3×3 Matrix
+
+int arr[3][3];  // Creates a 3-row, 3-column integer array
+✅ This means:
+arr[0][0] → First element
+arr[2][2] → Last element
+
