17aeb316d340987f33c23400ed95a84cf77732f9
Mahima Chavalam <sussybakka69696@gmail.com>
2025-02-09 17:55:37 +0530

    Update regular.cpp

--- Strings/ Declaring and Initializing Strings/regular.cpp
+++ Strings/ Declaring and Initializing Strings/regular.cpp
@@ -1,1 +1,12 @@
+#  Using char[] (C-Style Strings)
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str1[] = "Hello";  // Implicitly null-terminated
+    char str2[6] = "World"; // Explicitly define size
+
+    cout << str1 << " " << str2 << endl;
+    return 0;
+}
 
