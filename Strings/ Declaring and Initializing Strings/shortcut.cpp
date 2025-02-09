51634f15d7e946bb5ca56b59207f38570012e7ea
Mahima Chavalam <sussybakka69696@gmail.com>
2025-02-09 17:55:10 +0530

    Create shortcut.cpp

---
+++ Strings/ Declaring and Initializing Strings/shortcut.cpp
@@ -0,0 +1,13 @@
+#  Using string Class (Modern C++ Approach)
+
+#include <iostream>
+#include <string>  // Required for string class
+using namespace std;
+
+int main() {
+    string str1 = "Hello";
+    string str2("World");
+
+    cout << str1 + " " + str2 << endl; // Concatenation
+    return 0;
+}
