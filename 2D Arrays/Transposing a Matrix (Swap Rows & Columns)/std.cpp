789a8a2b38265d0e1bca1c113ca711bed598e192
Mahima Chavalam <sussybakka69696@gmail.com>
2025-02-09 18:17:10 +0530

    Create std.cpp

---
+++ 2D Arrays/Transposing a Matrix (Swap Rows & Columns)/std.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    int transpose[3][3];
+
+    // Transpose logic: Swap arr[i][j] with arr[j][i]
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            transpose[j][i] = arr[i][j];
+        }
+    }
+
+    cout << "Transposed Matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+o/p :
+1  4  7  
+2  5  8  
+3  6  9  
