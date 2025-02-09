89ca9910f4cd92d61ed65377dbe582d96398de16
Mahima Chavalam <sussybakka69696@gmail.com>
2025-02-09 17:37:23 +0530

    Create Regular.cpp

---
+++ Sorting an array/Regular.cpp
@@ -0,0 +1,27 @@
+# traditional way Using Loops (Bubble Sort - Traditional Way)
+
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]); // Swap elements
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
