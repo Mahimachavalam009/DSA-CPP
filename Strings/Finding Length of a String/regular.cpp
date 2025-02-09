0e6031e55bc6662e0b30049c2b261e9f4337088f
Mahima Chavalam <sussybakka69696@gmail.com>
2025-02-09 17:56:51 +0530

    Create regular.cpp

---
+++ Strings/Finding Length of a String/regular.cpp
@@ -0,0 +1,13 @@
+#  Using length()
+✅ str.length() returns the number of characters in a string.
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "Hello World!";
+    cout << "Length: " << str.length() << endl;
+    return 0;
+}
