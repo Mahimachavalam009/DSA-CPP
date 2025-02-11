d343f1155b5430c5f97cbf09b668db9655b29ea6
Mahima Chavalam <sussybakka69696@gmail.com>
2025-02-11 13:04:37 +0530

    Create btoc.cpp

---
+++ numbersystem/Binary to Decimal Conversion/btoc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int binaryToDecimal(int n) {
+    int decimal = 0, i = 0;
+    while (n > 0) {
+        int lastDigit = n % 10;
+        decimal += lastDigit * pow(2, i);
+        n /= 10;
+        i++;
+    }
+    return decimal;
+}
+
+int main() {
+    int binary = 1011;
+    cout << "Decimal: " << binaryToDecimal(binary);
+    return 0;
+}
